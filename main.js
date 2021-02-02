37adf4ad81d9756bd8fb67aa0ba7f45191bbad81
BluberryPickle <milinddinesh@gmail.com>
2021-02-02 18:17:57 +0530

    added js functions to set properties

--- main.js
+++ main.js
@@ -1,3 +1,26 @@
+//function to set the properties of menu cards.
+function properties() {
+    //Lists of items and cost respectively.
+    var items = ['Al-Faham','Chicken Curry','Fish Curry']; 
+    var cost = ['₹400','₹300','₹200'];
+    var count;
+    for (count = 1 ; count < 4 ; count++ ){ //Loop to set the properties of each card.
+        var box = document.getElementById('box'+count);
+        box.style.height='400px';
+        var img_div = document.getElementById('img'+count+'_text');
+        var para = document.createElement('p');
+        img_div.appendChild(para);
+        para.style = " font-size :30px";
+        var text = document.createTextNode(items[count-1]);
+        para.appendChild(text)
+        para = document.createElement('p');
+        img_div.appendChild(para);
+        text = document.createTextNode(cost[count-1]);
+        para.appendChild(text)
+    }
+}
+
+//Function to replace html contents
 function replace() {
     document.getElementById("container").innerHTML=" ";
     var container = document.getElementById('container');
@@ -16,5 +39,11 @@ function replace() {
         img.src="images/"+inum+".jpg";
         img.width="100%";
         img.style="height: 243px";
+        var text_div = document.createElement('div');
+        box.appendChild(text_div);
+        var cid = 'img'+count+'_text';
+        text_div.className=cid;
+        text_div.id = cid;
     }
+    properties()
 }
