173c9099c673c8e583ec587bc237a9f70b716b4a
BluberryPickle <milinddinesh@gmail.com>
2021-02-10 02:19:32 +0530

    retrieve function sets page now

--- cart.js
+++ cart.js
@@ -1,4 +1,16 @@
 function retrieve() {
     cart = JSON.parse(sessionStorage.getItem('cart'));
-    alert(cart['add1'])
+    var i ;
+    var counter = 1;
+    for (i=0; i < 3; i++){
+        var container = document.getElementById('container');
+        var iblock = document.createElement('div');
+        container.appendChild(iblock);
+        iblock.className = 'iblock';
+        /*iblock.id = "add"+counter ; */
+        img = document.createElement('img');
+        iblock.appendChild(img);
+        img.src = '/images/img'+counter+'.jpg';
+        counter ++
+    }
 }
