1910d2ea6e3639932972bf3399bd889af2777c88
BluberryPickle <milinddinesh@gmail.com>
2021-02-11 01:55:03 +0530

    modified function

--- cart.js
+++ cart.js
@@ -1,16 +1,28 @@
 function retrieve() {
+    var items = ['Al-Faham','Chicken Curry','Fish Curry'];
+    var place = ['Cook door','Copper Spoon','Pepper Restaurant ']
     cart = JSON.parse(sessionStorage.getItem('cart'));
     var i ;
-    var counter = 1;
-    for (i=0; i < 3; i++){
-        var container = document.getElementById('container');
-        var iblock = document.createElement('div');
-        container.appendChild(iblock);
-        iblock.className = 'iblock';
-        /*iblock.id = "add"+counter ; */
-        img = document.createElement('img');
-        iblock.appendChild(img);
-        img.src = '/images/img'+counter+'.jpg';
-        counter ++
+    var container = document.getElementById('container');
+    for (i=1; i < 4; i++){
+        if (cart['add'+i]!=undefined){
+            var iblock = document.createElement('div');
+            container.appendChild(iblock);
+            iblock.className = 'iblock';
+            img = document.createElement('img');
+            iblock.appendChild(img);
+            img.src = '/images/img'+i+'.jpg';
+            var div = document.createElement('div');
+            iblock.appendChild(div)
+            div.className='text';
+            var h1 = document.createElement('h1');
+            div.appendChild(h1);
+            h1.innerHTML=items[i-1];
+            var p = document.createElement('p');
+            div.appendChild(p);
+            p.style.fontSize='25px';
+            p.innerHTML=place[i-1];
+
+        }
     }
 }
