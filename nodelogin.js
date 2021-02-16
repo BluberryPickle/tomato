fa0c2de6e6781864718d45fb8f67b2ea0b8cba8a
BluberryPickle <milinddinesh@gmail.com>
2021-02-17 01:01:40 +0530

    added code to connect to db

--- nodelogin.js
+++ nodelogin.js
@@ -3,3 +3,20 @@ var express = require('express');
 var session = require('express-session');
 var bodyParser = require('body-parser');
 var path = require('path');
+
+var connection = mysql.createConnection({
+	host     : 'localhost',
+	user     : 'root',
+	password : 'pass',
+	database : 'nodelogin'
+});
+
+var app = express();
+app.use({
+    secret: 'secret',
+	resave: true,
+	saveUninitialized: true
+})
+
+app.use(bodyParser.urlencoded({extended : true}));
+app.use(bodyParser.json());
