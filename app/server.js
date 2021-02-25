0364777fdbd491eb81204df194c45fa64af0e1ab
BluberryPickle <milinddinesh@gmail.com>
2021-02-25 19:31:37 +0530

    removed unnecessary file

--- app/server.js
+++
@@ -1,6 +0,0 @@
-const express = require('express')
-const app = express()
-const port = 3000
-
-app.use(express.static('nodelogin'))
-app.listen(port, () => console.log(`listening on port ${port}!`))
